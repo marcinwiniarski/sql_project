d093c4e301b1904c801f01ac25bfddaae0805beb
marcinwiniarski <winiarski.mw@pg.com>
2022-10-11 17:21:31 +0200

    Updated query and fixed join issue

--- sql_query1.sql
+++ sql_query1.sql
@@ -1,12 +1,20 @@
-SELECT COALESCE(a.dimension_1, b.dimension_1) AS [dimension_1],
+WITH base AS
+  (SELECT dimension_1,
+          measure_1,
+          NULL AS measure_2
+   FROM [Table A]
+   UNION SELECT dimension_1,
+                NULL AS measure_1,
+                measure_2
+   FROM [Table B])
+SELECT base.dimension_1,
        map.correct_dimension_2 AS dimension_2,
        COALESCE(SUM(measure_1), 0) measure_1,
        COALESCE(SUM(measure_2), 0) measure_2
-FROM [Table A] a
-FULL  JOIN [Table B] b ON a.dimension_1 = b.dimension_1
+FROM base
 INNER JOIN
   (SELECT DISTINCT dimension_1,
                    correct_dimension_2
-   FROM [Table MAP]) map ON COALESCE(a.dimension_1, b.dimension_1) = map.dimension_1
-GROUP BY COALESCE(a.dimension_1, b.dimension_1),
+   FROM [Table MAP]) map ON base.dimension_1 = map.dimension_1
+GROUP BY base.dimension_1,
          map.correct_dimension_2
